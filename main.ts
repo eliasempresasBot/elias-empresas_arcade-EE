cdcc3fdfc34188a5a1ca08236663c8829456bfae
ELIAS EMPRESAS <eliasempresas0@gmail.com>
2025-03-06 18:51:11 -0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let ELIAS_EMPRESAS = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f 2 2 2 2 2 2 f f f . . 
+    . . f f 2 2 2 2 2 2 2 2 2 f . . 
+    . . f 2 2 f f f f f f 2 2 f . . 
+    . . f f f f 2 2 2 2 f f f f . . 
+    . f f 2 f b f 2 2 f b f 2 f f . 
+    . f 2 2 2 1 f 2 2 f 1 2 2 2 f . 
+    . . f 2 2 2 2 2 2 2 2 2 2 f . . 
+    . . . f 2 2 2 2 2 2 2 2 f . . . 
+    . . 2 2 f 2 2 2 2 2 2 f 2 2 . . 
+    . . 2 2 f 2 2 2 2 2 2 f 2 2 . . 
+    . . 2 2 f 2 2 2 2 2 2 f 2 2 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . 2 2 . . 2 2 . . . . . 
+    `, SpriteKind.Player)
